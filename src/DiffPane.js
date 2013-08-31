7ea8d0e244ad6f0940bd6e7682b636a638097b84
Justin Hewlett <hewlett.jus@gmail.com>
2013-08-31 11:03:51 -0600

    moved some of the javascript to a separate file. improved html and css styling. added some experimental code to deal with blank lines and spacing.

---
+++ src/DiffPane.js
@@ -0,0 +1,82 @@
+function contains(array, line) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].line === line) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function doDiff() {
+    var text1 = document.getElementById('original').value;
+    var text2 = document.getElementById('edited').value;
+
+    var results = diff(text1, text2);
+
+    var text1Lines = text1.split('\n');
+    var text2Lines = text2.split('\n');
+
+    //todo: why is this needed?
+    for (var l = 0; l < text1Lines.length; l++) {
+        if (text1Lines[l] == '') {
+            text1Lines[l] = ' ';
+        }
+    }
+
+    for (l = 0; l < text2Lines.length; l++) {
+        if (text2Lines[l] == '') {
+            text2Lines[l] = ' ';
+        }
+    }
+
+    for (var c = 0; c < results.deleted.length; c++) {
+        var line = results.deleted[c].line;
+        if (line < text2Lines.length && !contains(results.added, line)) {
+            text2Lines.splice(line, 0, '');
+            for(var e = 0; e < results.added.length; e++) {
+                if (results.added[e].line >= line) {
+                    results.added[e].line++;
+                }
+            }
+        }
+    }
+
+    for (c = 0; c < results.added.length; c++) {
+        line = results.added[c].line;
+        if (line < text1Lines.length && !contains(results.deleted, line)) {
+            text1Lines.splice(line, 0, '');
+            for(var d = 0; d < results.deleted.length; d++) {
+                if (results.deleted[d].line >= line) {
+                    results.deleted[d].line++;
+                }
+            }
+        }
+    }
+
+    var deletedText = "";
+    for(var i = 0; i < text1Lines.length; i++) {
+        if (contains(results.deleted, i)) {
+            deletedText += '<span class="deleted">';
+        }
+        deletedText += text1Lines[i];
+        if (contains(results.deleted, i)) {
+            deletedText += '</span>';
+        }
+        deletedText += "<br>";
+    }
+
+    var addedText = "";
+    for(i = 0; i < text2Lines.length; i++) {
+        if (contains(results.added, i)) {
+            addedText += '<span class="inserted">';
+        }
+        addedText += text2Lines[i];
+        if (contains(results.added, i)) {
+            addedText += '</span>';
+        }
+        addedText += "<br>";
+    }
+
+    document.getElementById('original_result').innerHTML = deletedText;
+    document.getElementById('edited_result').innerHTML = addedText;
+}
--- src/SourceDiff.js
+++ src/SourceDiff.js
@@ -18,6 +18,18 @@ function diff(s1, s2) {
     var s1Lines = s1.split('\n');    //todo: handle \r?
     var s2Lines = s2.split('\n');
 
+    for (var l = 0; l < s1Lines.length; l++) {
+        if (s1Lines[l] == '') {
+            s1Lines[l] = ' ';
+        }
+    }
+
+    for (l = 0; l < s2Lines.length; l++) {
+        if (s2Lines[l] == '') {
+            s2Lines[l] = ' ';
+        }
+    }
+
     var prefixLines = trim(s1Lines, s2Lines);
 
     var matrix = createMatrix(s1Lines, s2Lines);
