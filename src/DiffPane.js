4422fd27be5092b0c70634b32a6e8751a1d6d131
Justin Hewlett <hewlett.jus@gmail.com>
2013-08-31 22:44:41 -0600

    refactoring, convert tabs to three spaces

--- src/DiffPane.js
+++ src/DiffPane.js
@@ -35,6 +35,35 @@ function lineUpText(text1, text2, results) {
     return [text1Lines, text2Lines];
 }
 
+function getStartingPos(results) {
+    var firstDelete = results.deleted.length > 0
+        ? results.deleted[0].line
+        : 0;
+
+    var firstAdd = results.added.length > 0
+        ? results.added[0].line
+        : 0;
+
+    var firstEdit = Math.min(firstDelete, firstAdd);
+
+    var startingPos = Math.max(0, firstEdit - 10);
+    return startingPos;
+}
+
+function getEndingPos(results, lines) {
+    var lastDelete = results.deleted.length > 0
+        ? results.deleted[results.deleted.length - 1].line
+        : 0;
+
+    var lastAdd = results.added.length > 0
+        ? results.added[results.added.length - 1].line
+        : 0;
+
+    var lastEdit = Math.max(lastDelete, lastAdd);
+
+    return Math.min(lines.length, lastEdit + 10);
+}
+
 function doDiff() {
     var text1 = document.getElementById('original').value;
     var text2 = document.getElementById('edited').value;
@@ -46,12 +75,16 @@ function doDiff() {
     var text1Lines = lines[0];
     var text2Lines = lines[1];
 
+    var startingPos = getStartingPos(results);
+    var text1EndingPos = getEndingPos(results, text1Lines);
+    var text2EndingPos = getEndingPos(results, text2Lines);
+
     var deletedText = '';
-    for(var i = 0; i < text1Lines.length; i++) {
+    for(var i = startingPos; i < text1EndingPos; i++) {
         if (contains(results.deleted, i)) {
             deletedText += '<span class="deleted">';
         }
-        deletedText += text1Lines[i];
+        deletedText += escapeHtml(text1Lines[i]).replace(/\t/g, '   ');
         if (contains(results.deleted, i)) {
             deletedText += '</span>';
         }
@@ -59,11 +92,11 @@ function doDiff() {
     }
 
     var addedText = '';
-    for(i = 0; i < text2Lines.length; i++) {
+    for(i = startingPos; i < text2EndingPos; i++) {
         if (contains(results.added, i)) {
             addedText += '<span class="inserted">';
         }
-        addedText += text2Lines[i];
+        addedText += escapeHtml(text2Lines[i]).replace(/\t/g, '   ');
         if (contains(results.added, i)) {
             addedText += '</span>';
         }
@@ -72,4 +105,19 @@ function doDiff() {
 
     document.getElementById('original_result').innerHTML = deletedText;
     document.getElementById('edited_result').innerHTML = addedText;
+}
+
+function escapeHtml(string) {
+    var entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+    };
+
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+        return entityMap[s];
+    });
 }
