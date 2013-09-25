c8d9a79f3e9ecd0993f4d0ffb1759226830978c1
Justin Hewlett <hewlett.jus@gmail.com>
2013-09-24 19:46:47 -0600

    moved deployment script into air folder. added self-signed certificate. Fixed diffCmd.html to work with relative directories.

---
+++ air/airDeploy.bat
@@ -0,0 +1,1 @@
+adt -package -storetype pkcs12 -keystore selfCert.p12 SourceDiff.air SourceDiff-app.xml diffCmd.html AIRAliases.js -C ..\lib bootstrap.min.css -C ..\src Diff.js DiffFormatter.js EditSet.js LineDiff.js LineFormatter.js style.css
